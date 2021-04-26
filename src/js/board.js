29913d839ef62ef72d95033e97e6dc285b46c6e9
holdennekt <ngryshchak@gmail.com>
2021-04-27 01:07:07 +0300

    add some arrs

--- src/js/board.js
+++ src/js/board.js
@@ -4,8 +4,13 @@ const gameBoard = {
 	'hisPly': 0,
 	'ply': 0,
 	'castlePerm': 0,
+    'enPas' : 0,
 };
 gameBoard.score = {
     'white' : 0,
     'black' : 0,
 };
+gameBoard.figList = [];
+gameBoard.figNum = [];
+gameBoard.moveList = [];
+gameBoard.moveListFirst = [];
