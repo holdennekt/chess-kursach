a35b2d68efcbf52081e5cefaa9494227aab29839
holdennekt <ngryshchak@gmail.com>
2021-05-17 00:47:12 +0300

    new scripts

--- src/script.js
+++ src/script.js
@@ -5,3 +5,19 @@ const block = document.querySelector('#container');
 block.addEventListener('click', clickedOnSquare);
 
 createBoard(block);
+
+const initBoardVars = () => {
+    for (const elem of gameBoard.history) {
+        elem = {
+            move: null,
+            fiftyMove: 0,
+            posKey: 0,
+        }
+    }
+    for (const elem of gameBoard.pvTable) {
+        elem = {
+            move: null,
+            posKey: 0,
+        }
+    }
+};
