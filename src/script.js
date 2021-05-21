417cd3ad8bd1a4ca558b7fe0272da69d9b56cece
Bohdan <bogdanpin2002@gmail.com>
2021-05-21 15:39:31 +0300

    init functions

--- src/script.js
+++ src/script.js
@@ -21,3 +21,21 @@ const initBoardVars = () => {
         }
     }
 };
+
+const initHashKeys = () => {
+    for (let i = 0; i < 14 * 120; i++) {
+        figKeys[i] = rand32();
+    }
+
+    sideKey = rand32();
+
+    for (let i = 0; i < 16; i++) {
+        castleKeys[i] = rand32();
+    }
+}
+
+const init = () => {
+    initHashKeys();
+}
+
+init();
