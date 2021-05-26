88fe9e08675327c3155265c79562213a2edfdd1e
holdennekt <ngryshchak@gmail.com>
2021-05-27 02:56:46 +0300

    check highlighting and async oromotion

--- src/js/gui.js
+++ src/js/gui.js
@@ -4,54 +4,38 @@ const userMove = {
     promoted: figs.empty,
 };
 
-const chosen = (click) => {
-    const clickedOn = click.target.id;
-    userMove.promoted = figs[clickedOn];
-    document.body.removeChild(document.querySelector('.promotion'));
+const chosen = async block => {
+    return new Promise(resolve => block.onclick = click => {
+        const clickedOn = click.target.id;
+        document.body.removeChild(document.querySelector('.promotion'));
+        resolve(figs[clickedOn]);
+    });
 };
 
-const suggestPromotion = () => {
-    let blockPromotion = document.createElement('div');
+const suggestPromotion = async () => {
+    const blockPromotion = document.createElement('div');
     blockPromotion.className = 'promotion';
     document.body.appendChild(blockPromotion);
     let list;
     if (gameBoard.side === colors.white) list = ['wQ', 'wR', 'wB', 'wN'];
     else list = ['bQ', 'bR', 'bB', 'bN'];
     for (const elem of list) {
-        let img = new Image();
+        const img = new Image();
         img.src = `icons/${figs[elem]}.png`;
         img.id = elem;
-        img.className = `suggestion`;
+        img.className = 'suggestion';
         blockPromotion.append(img);
     }
-    blockPromotion.addEventListener('click', chosen);
+    return await chosen(blockPromotion);
 };
 
-const parseMove = (from, to, promoted = figs.empty) => {
-    const start = gameBoard.moveListStart[gameBoard.ply];
-    const end = gameBoard.moveListStart[gameBoard.ply + 1];
-    let found = false, move;
-    for (let index = start; index < end; index++) {
-        move = gameBoard.moveList[index];
-        const fromI = move.from[0], fromJ = move.from[1];
-        const toI = move.to[0], toJ = move.to[1];
-        if (from[0] === fromI && from[1] === fromJ &&
-            to[0] === toI && to[1] === toJ) {
-            if (promoted !== figs.empty) {
-                if (move.promoted === promoted) {
-                    console.log('PROMOTED TO', revFigs[promoted]);
-                    found = true;
-                    break;
-                }
-                continue;
-            }
-            found = true;
-            break;
-        }
-    }
-    if (found) {
-        if (!isMoveLegal(move)) {
-            return noMove();
+const parseMove = async (from, to) => {
+    const move = findMove({ from, to });
+    if (!checkObjectsEqual(move, noMove())) {
+        if (grid[from[0]][from[1]] === 1 && from[0] === 1 ||
+            grid[from[0]][from[1]] === 7 && from[0] === 6) {
+            const promotion = await suggestPromotion();
+            move.promoted = promotion;
         }
         return move;
     }
@@ -74,39 +58,23 @@ const selectSquares = (i, j, suggest = []) => {
         document.querySelectorAll(str)[0].className += ' selected';
         for (const elem of suggest) {
             str = `.rank${elem.to[0]}.file${elem.to[1]}`;
+            const block = document.querySelectorAll(str)[0];
             if (elem.captured === figs.empty) {
-                document.querySelectorAll(str)[0].className += ' suggested';
-            }
-            else document.querySelectorAll(str)[0].className += ' suggestedCapture';
+                block.className += ' suggested';
+            } else block.className += ' suggestedCapture';
         }
     }
 };
 
-const clickedOnSquare = (i, j) => {
-    // console.log(`clicked on square_${i}${j}`);
-    if (userMove.from[0] !== -1 && userMove.from[1] !== -1) {
-        userMove.to[0] = i, userMove.to[1] = j;
-        makeUserMove();
-    }
-}
-
-const clickedOnFigure = (i, j) => {
-    selectSquares(i, j);
-    // console.log(`clicked on figure ${grid[i][j]} at square_${i}${j}`);
-    if (userMove.from[0] === -1 && userMove.from[1] === -1 &&
-        figCol[grid[i][j]] === gameBoard.side) {
-        userMove.from[0] = i, userMove.from[1] = j;
-    } else if (userMove.from[0] === -1 && userMove.from[1] === -1 &&
-                figCol[grid[i][j]] !== gameBoard.side) {
-        return;
-    } else {
-        const figFrom = grid[userMove.from[0]][userMove.from[1]];
-        if (figCol[grid[i][j]] === figCol[figFrom]) {
-            userMove.from[0] = i, userMove.from[1] = j;
-        } else userMove.to[0] = i, userMove.to[1] = j;
-    }
-    makeUserMove();
-}
+const toggleCheckKing = (mode = '') => {
+    console.log('TOGGLE CALLED');
+    const king = gameBoard.figList[figIndex(kings[gameBoard.side], 0)];
+    const str = `.figure.rank${king[0]}.file${king[1]}`;
+    const kingSq = document.querySelectorAll(str)[0];
+    if (mode === 'on') kingSq.classList.add('check');
+    else if (mode === 'off') kingSq.classList.remove('check');
+    else throw new Error('should be argument either "on" or "off"');
+};
 
 const removeGuiFig = sq => {
     const str = `.figure.rank${sq[0]}.file${sq[1]}`;
@@ -116,7 +84,7 @@ const removeGuiFig = sq => {
 
 const addGuiFig = (sq, fig) => {
     const i = sq[0], j = sq[1];
-    let img = new Image();
+    const img = new Image();
     img.src = `icons/${fig}.png`;
     img.className = `figure rank${i} file${j}`;
     document.querySelector('#container').append(img);
@@ -127,8 +95,7 @@ const moveGuiFig = move => {
         let epRemove;
         if (gameBoard.side === colors.white) {
             epRemove = [move.to[0] - 1, move.to[1]];
-        }
-        else epRemove = [move.to[0] + 1, move.to[1]];
+        } else epRemove = [move.to[0] + 1, move.to[1]];
         removeGuiFig(epRemove);
     } else if (move.captured !== figs.empty) {
         removeGuiFig(move.to);
@@ -162,48 +129,73 @@ const moveGuiFig = move => {
     }
 };
 
-const makeUserMove = () => {
+const makeUserMove = async () => {
     const from = userMove.from, to = userMove.to;
-    if (from[0] !== -1 && from[1] !== -1) {
+    if (notEmptyMove(from[0], from[1])) {
         const start = gameBoard.moveListStart[gameBoard.ply];
         const end = gameBoard.moveListStart[gameBoard.ply + 1];
         const suggest = [];
         for (let index = start; index < end; index++) {
-            let move = gameBoard.moveList[index];
-            if (from[0] === move.from[0] && from[1] === move.from[1] &&
-                isMoveLegal(move)) {
+            const move = gameBoard.moveList[index];
+            if (checkArrsEqual(from, move.from) && isMoveLegal(move)) {
                 suggest.push(move);
             }
         }
         selectSquares(from[0], from[1], suggest);
     }
-    if (from[0] !== -1 && from[1] !== -1 &&
-        to[0] !== -1 && to[1] !== -1) {
-        if (grid[from[0]][from[1]] === 1 && from[0] === 1 ||
-            grid[from[0]][from[1]] === 7 && from[0] === 6) {
-            suggestPromotion();
-        }
-        const parsed = parseMove(from, to, userMove.promoted);
-        console.log(parsed);
-        if (parsed.from[0] !== -1 && parsed.from[1] !== -1 &&
-            parsed.to[0] !== -1 && parsed.to[1] !== -1) {
+    if (notEmptyMove(from[0], from[1], to[0], to[1])) {
+        const parsed = await parseMove(from, to);
+        // console.log(parsed);
+        const t = [parsed.from[0], parsed.from[1], parsed.to[0], parsed.to[1]];
+        if (notEmptyMove(t)) {
+            toggleCheckKing('off');
             makeMove(parsed);
             logGrid();
             moveGuiFig(parsed);
+            king = gameBoard.figList[figIndex(kings[gameBoard.side], 0)];
+            if (isSqAttackedBySide(king, gameBoard.side ^ 1)) {
+                toggleCheckKing('on');
+            }
         }
         deselectSquares();
-        console.log('user move' + from[0] + from[1] + ' -> ' + to[0] + to[1]);
+        // console.log('user move' + from[0] + from[1] + '->' + to[0] + to[1]);
         userMove.from = [-1, -1];
         userMove.to = [-1, -1];
         generateMoves();
     }
 };
 
+const clickedOnSquare = (i, j) => {
+    // console.log(`clicked on square_${i}${j}`);
+    if (notEmptyMove(userMove.from[0], userMove.from[1])) {
+        userMove.to[0] = i, userMove.to[1] = j;
+        makeUserMove();
+    }
+};
+
+const clickedOnFigure = (i, j) => {
+    selectSquares(i, j);
+    // console.log(`clicked on figure ${grid[i][j]} at square_${i}${j}`);
+    if (userMove.from[0] === -1 && userMove.from[1] === -1 &&
+        figCol[grid[i][j]] === gameBoard.side) {
+        userMove.from[0] = i, userMove.from[1] = j;
+    } else if (userMove.from[0] === -1 && userMove.from[1] === -1 &&
+                figCol[grid[i][j]] !== gameBoard.side) {
+        return;
+    } else {
+        const figFrom = grid[userMove.from[0]][userMove.from[1]];
+        if (figCol[grid[i][j]] === figCol[figFrom]) {
+            userMove.from[0] = i, userMove.from[1] = j;
+        } else userMove.to[0] = i, userMove.to[1] = j;
+    }
+    makeUserMove();
+};
+
 const clicked = click => {
     const x = click.pageX - click.path[1].getBoundingClientRect().left;
     const y = click.pageY - click.path[1].getBoundingClientRect().top;
-    const i = Math.floor(y/100);
-    const j = Math.floor(x/100);
+    const i = Math.floor(y / 100);
+    const j = Math.floor(x / 100);
     const targetClass = click.target.className;
     if (targetClass.includes('square')) clickedOnSquare(i, j);
     else if (targetClass.includes('figure')) clickedOnFigure(i, j);
